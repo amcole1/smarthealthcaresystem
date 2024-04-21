07b2a643074a2cff3c04a69cd2ee506fa5ff8c23
amcole1 <amcole1@radford.edu>
2024-04-21 19:23:14 -0400

    Update appointment.js

--- models/appointment.js
+++ models/appointment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const AppointmentSchema = new mongoose.Schema({
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'doctor', required: true },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
   date: { type: Date, required: true },
   time: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null } // Allows null initially
